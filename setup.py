35decd650642968158ce2761504dddd708f6d60b
arshadzahangirchowdhury <arshad.zahangir.bd@gmail.com>
2022-07-07 13:59:36 -0400

    tuner files and results

--- setup.py
+++ setup.py
@@ -8,17 +8,17 @@ from setuptools import setup, find_packages
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
-    name='aimos',
-    url='https://github.com/arshadzahangirchowdhury/AIMOS-D',
+    name='VOC-Net',
+    url='https://github.com/arshadzahangirchowdhury/VOC-Net',
     author='M Arshad Zahangir Chowdhury',
     author_email='arshad.zahangir.bd@gmail.com',
     # Needed to actually package something
     packages=find_packages(exclude=['test']),
     # Needed for dependencies
     install_requires=['numpy', 'pandas', 'scipy', 'h5py', 'matplotlib', 'opencv-python',\
-                      'scikit-image','scikit-learn', 'seaborn' ,'ipython', 'umap-learn', 'tensorflow'],
+                      'scikit-image','scikit-learn', 'seaborn' ,'ipython', 'tensorflow', 'tensorflow'],
     version="1.0",
     license='BSD',
-    description='AIMOS-D: Artificial Intelligence Methods for Organizing Spectral Data',
+    description='VOC-Net: Volatile Organic Compounds Classification Network',
 #     long_description=open('README.md').read(),
 )
